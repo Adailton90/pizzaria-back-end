e5a3be7056d62cc83dd9d1b3f9e7cb200d3fa965
Adailton Silva <adailton.silva10@live.com>
2020-05-29 11:24:15 -0300

    .

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -9,7 +9,7 @@ router.get('/users/:id',userController.listUsersID)
 router.post('/user', userController.createUser)
 
 router.put('/users/:id', login, userController.updateUser)
-router.delete('/user/:id',userController.deleteUser)
+router.delete('/user/:id', userController.deleteUser)
 router.post('/user/login',userController.loginUser)
 
 module.exports = router
