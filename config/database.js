9acca7f51a0b8569d50f6d854c4a86e337a2d32e
Adailton Silva <adailton.silva10@live.com>
2020-05-29 18:27:50 -0300

    mudança dos parametros do banco

--- config/database.js
+++ config/database.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
+
 module.exports = {
-  username: 'pizzaria',
-  password: '123pizzaria',
-  database: 'pizzaria_db',
+  username: process.env.MYSQL_USERNAME,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
   host: '127.0.0.1',
   dialect: 'mysql',
 }
