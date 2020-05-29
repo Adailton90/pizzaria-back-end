82a4f1416198b7bdda9e106fc522ce79509e7ccb
Adailton Silva <adailton.silva10@live.com>
2020-05-29 18:33:48 -0300

    no message

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
 
 module.exports = {
-  username: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB,
-  host: '127.0.0.1',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DATABASE,
+  host: process.env.DB_HOST,
   dialect: 'mysql',
 }
